282e515cd3f42695a0d3f58452d14f3a62697b31
akhil1095 <Akhil.Thakkar@MyGeorgian.ca>
2015-10-16 16:25:56 -0600

    final portfolio

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index' );
+});
+
+router.get('/about', function(req, res, next) {
+   res.render('about'); 
+});
+router.get('/projects', function(req, res, next) {
+   res.render('projects'); 
+});
+/* GET ABOUT page */
+router.get('/services', function(req, res, next) {
+   res.render('Services'); 
+});
+router.get('/contact', function(req, res, next) {
+   res.render('contact'); 
+});
+
+
+
+module.exports = router;
